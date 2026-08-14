90b91929a24cafa70b47a0d46aaad1649171b688
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-08-15 02:52:39 +0700

    refactor: remove dead persistence wrapper

--- backend/src/infrastructure/persistence.ts
+++
@@ -1,9 +0,0 @@
-import { flushActivityStore } from "../modules/activity/store.js";
-import { flushOutboundPolicyPersistence } from "../modules/messages/outbound-policy.js";
-import { flushRecipientStore } from "../modules/recipients/store.js";
-import { closeDatabase } from "./database.js";
-
-export async function flushPersistence(): Promise<void> {
-  await Promise.all([flushActivityStore(), flushRecipientStore(), flushOutboundPolicyPersistence()]);
-  closeDatabase();
-}
